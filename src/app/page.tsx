475359280e661009214e2d871a304ef5d66f32ca
HamedSadim1 <hamid.sadim@outlook.com>
2024-01-29 20:04:07 +0100

    add icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,48 @@
-import Navbar from "@/components/Navbar";
 import PageTitle from "@/components/PageTitle";
+import Image from "next/image";
+import HeroImage from "@/images/utkarsh-img.webp";
+import { GithubIcon, Email, LinkedinIcon } from "@/components/Icons";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <PageTitle title="Home" />
+      <div className="w-full  flex py-28">
+        {/* left Hero Section  */}
+        <div className="flex w-1/2 flex-col gap-3 items-center">
+          <Image
+            src={HeroImage}
+            alt="user avtar"
+            className="w-[192px] h-[192px] rounded-full"
+          />
+          <h1 className="text-2xl font-semibold">Hamid Sadim</h1>
+          <p>Hey my name is Hamed Sadim and Iam a Frontend Developer</p>
+          <div className="flex gap-2 items-center text-3xl text-green-500/80">
+            <Link href="/" target="_blank" className="hover:opacity-60">
+              <GithubIcon />
+            </Link>
+            <Link href="/" target="_blank" className="hover:opacity-60">
+              <Email />
+            </Link>
+            <Link href="/" target="_blank" className="hover:opacity-60">
+              <LinkedinIcon />
+            </Link>
+          </div>
+        </div>
+        <div className="w-1/2 flex flex-col gap-4 text-lg">
+          {/* right dex */}
+          <p>
+            Welcome to my portfolio! Im a versatile frontend developer
+            specializing in crafting engaging and responsive web applications.
+          </p>
+          <p>
+            With a strong command of React.js, Tailwind CSS, and TypeScript,
+            along with a solid foundation in sorting algorithms, Im dedicated to
+            building visually stunning and high-performance user interfaces.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
