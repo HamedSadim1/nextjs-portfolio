226e4478aa92252f64bb81b69b4699dac374b775
HamedSadim1 <hamid.sadim@outlook.com>
2024-01-30 12:27:49 +0100

    refactor provider and layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Provider from "@/components/Provider/Provider";
+
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/Provider/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Provider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
         <body className={`${inter} w-full h max-h-screen items-start px-2`}>
           <Navbar />
           <div className="max-w-5xl mx-auto pt-4 ">{children}</div>
         </body>
-      </Provider>
+      </ThemeProvider>
     </html>
   );
 }
